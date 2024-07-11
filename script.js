2a76a8df5ac42a5b4384592ba7993d4a164a9cb6
ThalissonM <manoelthalisson28@gmail.com>
2024-07-10 22:19:20 -0300

    Add javascript
    Criado sistema de pesquisa para a barra de pesquisa usando Js, é possível pesquisar por titulo da hq

---
+++ script.js
@@ -0,0 +1,33 @@
+const searchInput = document.getElementById('search-here');
+
+searchInput.addEventListener('input', (event) => {
+    const value = formatString(event.target.value);
+    
+    const items = document.querySelectorAll('.list-hqs-completed .item')
+    const noResults = document.getElementById('no-results');
+
+    let hasResults = false;
+    
+    items.forEach(item =>{
+        const itemTitle = item.querySelector('.tittle').textContent;
+        if(formatString(itemTitle).indexOf(value) !== -1) {
+            item.style.display = 'flex';
+            hasResults = true;
+        }
+        else{
+            item.style.display = 'none';
+        }
+    })
+    if(hasResults){
+        noResults.style.display = 'none';
+    }
+    else{
+        noResults.style.display = 'block';
+    }
+});
+function formatString(value){
+    return value
+        .toLowerCase()
+        .trim();
+        
+}

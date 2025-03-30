21f8597b1b1d1798d3795c11abc2a9afdf49b855
ThalissonM <manoelthalisson28@gmail.com>
2025-03-29 22:52:35 -0300

    Update home.py
    Removendo apenas os prints

--- routes/home.py
+++ routes/home.py
@@ -6,13 +6,13 @@ def home():
     pastas = []
 
     itens = os.listdir(caminho)
-    print(itens)
+    # print(itens)
 
     # Para cada item na lista de itens
     for item in itens:
         # Junta o caminho 'caminho' com o nome do item para formar o caminho completo
         caminho_completo = os.path.join(caminho, item)
-        print(caminho_completo)
+        # print(caminho_completo)
         
         # Verifica se o caminho completo corresponde a uma pasta (diretório)
         if os.path.isdir(caminho_completo):

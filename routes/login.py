781257343a7d2530d6b9e1066545df5eb4b47b46
ThalissonM <manoelthalisson28@gmail.com>
2025-05-25 19:53:09 -0300

    Tentando fazer ususarios com json

--- routes/login.py
+++ routes/login.py
@@ -1,11 +1,20 @@
 from flask import render_template, request, redirect, url_for, flash
+import json
+import os
 
 def login():
-    if request.method == 'POST':
-        
+    def procurar_usuario(user, senha):
+        caminho_base = os.path.dirname(__file__)
+        caminho_json = os.path.join(caminho_base,'usuarios.json')
+        with open(caminho_json, 'r', encoding='utf-8') as f: dados = json.load(f)
+        print(f"Dados do json :{dados}")
+
+    procurar_usuario('admin','admin')
+
+    if request.method == 'POST':        
         usuario = request.form.get('usuario')
         senha = request.form.get('senha')
-        print(usuario, senha)
+        # print(usuario, senha)
 
         if usuario == 'admin' and senha == 'admin':
             return redirect(url_for('home'))

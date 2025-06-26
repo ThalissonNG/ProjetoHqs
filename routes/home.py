91299c339865b34da0f0ee446bac33edd22b35bf
ThalissonM <manoelthalisson28@gmail.com>
2025-06-26 16:49:12 -0300

    lib mega
    Feito o primeiro teste da lib do mega, deu erro usando o meu e-mail prinicipal, o manoel.lifer@gmail.com, estou agora migrando todos os dados para o manoelthalisson28@gmail.com

--- routes/home.py
+++ routes/home.py
@@ -1,5 +1,15 @@
-import os
+from mega import Mega
 from flask import render_template
 
 def home():
+    mega = Mega()
+    m = mega.login(email= 'manoelthalisson28@gmail.com', password='Liferkill123*')
+
+    detils = m.get_user()
+    print(detils['name'])
+    print(detils['email'])
+    arquivos = m.get_files()
+    print(arquivos)
+    # mega.login()
+
     return render_template('home.html')
